4a21c3c13a3e2946ef3c4f4f962b9abbb63c5035
RoxanaCortez <00502211@uca.edu.sv>
2019-03-16 01:19:01 -0600

    integrantes de app Contador de clientes

--- app/src/main/java/com/roxyapps/roxana/cartadepresentacin/Clientes.java
+++ app/src/main/java/com/roxyapps/roxana/cartadepresentacin/Clientes.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+// Integrantes : 00507511, 00502211, 00013812
+
 public class Clientes extends AppCompatActivity {
 
     private Button entran;
